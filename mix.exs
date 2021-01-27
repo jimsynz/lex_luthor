64cf9a634fb5ec8d2697a0c99ab4dab75c354269
James Harton <james@harton.nz>
2021-01-28 08:51:37 +1300

    chore: release version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LexLuthor.Mixfile do
   def project do
     [
       app: :lex_luthor,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.0",
       description:
         "LexLuthor is a Lexer in Elixir (say that 10 times fast) which uses macros to generate a reusable lexers. Good times.",
